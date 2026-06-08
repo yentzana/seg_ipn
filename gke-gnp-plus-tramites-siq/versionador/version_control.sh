9fb92e4ad0a5b87a8d2284bb9aef0adb37e193f1
yentzana <yentzana>
2026-06-08 11:41:54 -0600

    borrado inicial
    borrado

--- gke-gnp-plus-tramites-siq/versionador/version_control.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VERSION_TEMPORAL=".$1.$2"
-RUTA_ARCHIVO="gradle.properties"
-PROPIEDAD="version"
-sed -i -e 's/\r//' -e "s/$PROPIEDAD=.*/&$VERSION_TEMPORAL/" $RUTA_ARCHIVO
-VERSION_SONAR=`cat $RUTA_ARCHIVO | grep ${PROPIEDAD} | cut -d '=' -f2 | tr -d '\r'`
-echo "version sonarQube $VERSION_SONAR"
